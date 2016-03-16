90479413346bea3de9f52d3f8f7f6b456208907c
PVCR <pvchennareddy@gmail.com>
2016-03-16 19:53:20 -0400

    removed uniformgrid

--- PVCR.DragDropExample/Content/NavTopPage.xaml.cs
+++ PVCR.DragDropExample/Content/NavTopPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PVCR.DragDropExample.Utils;
 
 namespace PVCR.DragDropExample.Content
 {
@@ -20,25 +22,73 @@ namespace PVCR.DragDropExample.Content
     /// </summary>
     public partial class NavTopPage : UserControl
     {
+        
         public NavTopPage()
         {
             InitializeComponent();
         }
+        //private void tabmap_Click(object sender, EventArgs e)
+        //{
+        //    Map m = new Map();
+        //    m.Show();
+        //    Application.Current.Windows[0].Close();
+
+
+        //}
+
+        //private void tabmain_Click(object sender, EventArgs e)
+        //{
+        //    MainWindow w = new MainWindow();
+        //    w.Show();
+        //    Application.Current.Windows[0].Close();
+
+        //}
+
+
+
         private void tabmap_Click(object sender, EventArgs e)
         {
-            Map m = new Map();
-            m.Show();
-            Application.Current.Windows[0].Close();
+            //MapPage m = new MapPage();
+            //m.Show();
+            //Application.Current.Windows[0].Close();
+
+            UniformGrid ct = UIHelper.FindChild<UniformGrid>(Application.Current.MainWindow, "bodyContainer");
 
 
+            if (ct != null)
+            {
+                ct.Children.Clear();
+                ct.Children.Add(GetMap());
+            }
+
+
+        }
+
+        private UIElement GetMap()
+        {
+            return new MapPage();
+        }
+
+        private UIElement GetMain()
+        {
+            return new ContentPage();
         }
 
         private void tabmain_Click(object sender, EventArgs e)
         {
-            MainWindow w = new MainWindow();
-            w.Show();
-            Application.Current.Windows[0].Close();
+            UniformGrid ct = UIHelper.FindChild<UniformGrid>(Application.Current.MainWindow, "bodyContainer");
+
+
+            if (ct != null)
+            {
+                ct.Children.Clear();
+                ct.Children.Add(GetMain());
+            }
 
         }
+
+
+
+
     }
 }
